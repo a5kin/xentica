d2cb0ca354009dc42e73df5622b2341d0e271a5b
Andrey <a5kin@users.noreply.github.com>
2017-05-25 01:56:55 +0300

    Basic GPU arrays are created.

--- hecate/core/base.py
+++ hecate/core/base.py
@@ -1,4 +1,12 @@
+import functools
+import operator
+
 import numpy as np
+
+import pycuda.autoinit
+import pycuda.driver as drv
+from pycuda.compiler import SourceModule
+import pycuda.gpuarray as gpuarray
 
 class BSCA(type):
     """
@@ -16,16 +24,23 @@ class CellularAutomaton(metaclass=BSCA):
 
     """
     def __init__(self, experiment_class):
-        self.viewport_buf = np.zeros((3, ), dtype=np.uint8)
+        self.dtype = np.uint8  # HARDCODED
+        self.buffers = [0] * 9  # HARDCODED
+        self.frame_buf = np.zeros((3, ), dtype=np.uint8)
+        self.size = experiment_class.size
+        cells_total = functools.reduce(operator.mul, self.size)
+        cells_total *= len(self.buffers) + 1
+        self.cells_gpu = gpuarray.zeros((cells_total,), dtype=self.dtype)
 
     def set_viewport(self, size):
-        w, h = size
-        self.viewport_buf = np.zeros((w * h * 3,), dtype=np.uint8)
+        self.width, self.height = w, h = size
+        self.frame_buf = np.zeros((w * h * 3,), dtype=np.uint8)
+        self.img_gpu = gpuarray.to_gpu(self.frame_buf)
 
     def step(self):
-        self.viewport_buf = np.random.randint(0, 255,
-                                              self.viewport_buf.shape,
-                                              dtype=np.uint8)
+        self.frame_buf = np.random.randint(0, 255,
+                                           self.frame_buf.shape,
+                                           dtype=np.uint8)
 
     def render(self):
-        return self.viewport_buf
+        return self.frame_buf
