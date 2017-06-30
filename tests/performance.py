a33667f291420ca29b1ed3c95434a3c364d2dee2
andrey <a5kin@users.noreply.github.com>
2017-06-30 16:35:42 +0300

    Performance test is added.

---
+++ tests/performance.py
@@ -0,0 +1,22 @@
+import time
+
+from examples.game_of_life import GameOfLife, GOLExperiment
+from hecate.utils.formatters import sizeof_fmt
+
+
+models = [
+    ("Conway's Life", GameOfLife, GOLExperiment),
+]
+num_steps = 10000
+
+
+if __name__ == "__main__":
+    for name, model, experiment in models:
+        ca = model(experiment)
+        start_time = time.time()
+        for j in range(num_steps):
+            ca.step()
+            time_passed = time.time() - start_time
+        speed = num_steps * ca.cells_num // time_passed
+        print("%s: %s cells/s" % (name, sizeof_fmt(speed)))
+        del ca
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,4 +2,11 @@
 cd ..
 coverage run --source hecate -m unittest discover -s tests
 echo ""
+echo "COVERAGE"
+echo "========"
 coverage report -m
+echo ""
+echo "PERFORMANCE"
+echo "==========="
+CURDIR=$(pwd)
+PYTHONPATH="$CURDIR:$PYTHONPATH" python3 tests/performance.py
