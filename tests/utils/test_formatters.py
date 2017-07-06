817fdb32b20c29000c809993a5723e9e6143cedb
andrey <a5kin@users.noreply.github.com>
2017-07-06 11:53:52 +0300

    Formatters are covered by unit tests.

---
+++ tests/utils/test_formatters.py
@@ -0,0 +1,22 @@
+import unittest
+
+from hecate.utils.formatters import sizeof_fmt
+
+
+class TestSizeFormatter(unittest.TestCase):
+
+    def test_less_kilo(self):
+        val = sizeof_fmt(345)
+        self.assertEqual(val, "345", "Less Kilo format is incorrect.")
+
+    def test_kilo(self):
+        val = sizeof_fmt(34567)
+        self.assertEqual(val, "34.57K", "Kilo format is incorrect.")
+
+    def test_peta(self):
+        val = sizeof_fmt(34567 * (10 ** 12))
+        self.assertEqual(val, "34.57P", "Peta format is incorrect.")
+
+    def test_yokta(self):
+        val = sizeof_fmt(34567 * (10 ** 21))
+        self.assertEqual(val, "34.57Y", "Yokta format is incorrect.")
