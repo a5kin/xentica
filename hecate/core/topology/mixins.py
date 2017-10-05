ec90c0897c382397d60a906e33a7833c6c047d17
Andrey <a5kin@users.noreply.github.com>
2017-10-05 23:47:32 +0300

    Getter/setter for dimensions are implemented.

--- hecate/core/topology/mixins.py
+++ hecate/core/topology/mixins.py
@@ -5,11 +5,20 @@ class DimensionsMixin:
 
     supported_dimensions = []
 
+    def __init__(self):
+        self._dimensions = None
+
     def allowed_dimension(self, num_dim):
         return num_dim in self.supported_dimensions
 
-    def set_dimensions(self, num_dim):
+    @property
+    def dimensions(self):
+        return self._dimensions
+
+    @dimensions.setter
+    def dimensions(self, num_dim):
         if not self.allowed_dimension(num_dim):
             msg = "%d-D %s is not supported."
             msg = msg % (num_dim, self.__class__.__name__)
             raise HecateException(msg)
+        self._dimensions = num_dim
