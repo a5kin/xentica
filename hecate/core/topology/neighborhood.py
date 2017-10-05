b69a09570421b14846e9a0c167d59f85ffe34eae
Andrey <a5kin@users.noreply.github.com>
2017-10-05 23:49:30 +0300

    Coords/state functions placeholders are implemented for Neighborhood

--- hecate/core/topology/neighborhood.py
+++ hecate/core/topology/neighborhood.py
@@ -6,7 +6,22 @@ class Neighborhood(DimensionsMixin):
     Base class for all types of neighborhood.
 
     """
+    num_neighbors = 0
+
+    def set_border(self, border_object):
+        self.border = border_object
+
+    def __len__(self):
+        return self.num_neighbors
 
 
 class MooreNeighborhood(Neighborhood):
     supported_dimensions = [2, ]
+    num_neighbors = 8
+
+    def neighbor_coords(self, index, coord_prefix, neighbor_prefix):
+        return ""
+
+    def neighbor_state(self, neighbor_index, state_index,
+                       coord_prefix, state_name):
+        return ""
