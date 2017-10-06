993764057c270d8251f781a5f6af497e102bdb99
Andrey <a5kin@users.noreply.github.com>
2017-10-07 00:51:28 +0300

    Dimensions setter of MooreNeighborhood is modified to N-dimensional case.

--- hecate/core/topology/neighborhood.py
+++ hecate/core/topology/neighborhood.py
@@ -16,8 +16,14 @@ class Neighborhood(DimensionsMixin):
 
 
 class MooreNeighborhood(Neighborhood):
-    supported_dimensions = [2, ]
-    num_neighbors = 8
+    supported_dimensions = list(range(1, 100))
+    num_neighbors = None
+
+    @Neighborhood.dimensions.setter
+    def dimensions(self, num_dim):
+        super_class = super(MooreNeighborhood, MooreNeighborhood)
+        super_class.dimensions.fset(self, num_dim)
+        self.num_neighbors = 3 ** num_dim - 1
 
     def neighbor_coords(self, index, coord_prefix, neighbor_prefix):
         return ""
