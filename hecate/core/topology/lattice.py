7764e87fc69c78b42ec75d325a26cf11528879e7
Andrey <a5kin@users.noreply.github.com>
2017-10-08 21:24:34 +0300

    N-dimensional coord-to-index function is implemented for OrthogonalLattice.

--- hecate/core/topology/lattice.py
+++ hecate/core/topology/lattice.py
@@ -6,16 +6,17 @@ class Lattice(DimensionsMixin):
     Base class for all lattices.
 
     """
+    width_prefix = "_w"
 
 
 class OrthogonalLattice(Lattice):
     supported_dimensions = list(range(1, 100))
 
-    def generate_index_to_coord(self, index_name, coord_prefix, width_prefix):
+    def index_to_coord_code(self, index_name, coord_prefix):
 
         def wrap_format(s):
             return s.format(x=coord_prefix, i=i,
-                            index=index_name, w=width_prefix)
+                            index=index_name, w=self.width_prefix)
 
         for i in range(self.dimensions):
             if i == 0:
@@ -27,3 +28,12 @@ class OrthogonalLattice(Lattice):
             else:
                 code += wrap_format("int {x}{i} = {index};\n")
         return code
+
+    def coord_to_index_code(self, coord_prefix):
+        summands = []
+        for i in range(self.dimensions):
+            summand = coord_prefix + str(i)
+            for j in range(i):
+                summand = self.width_prefix + str(j) + " * " + summand
+            summands.append(summand)
+        return " + ".join(summands)
