0f5356bf361ccd6130113880f360c77623989f20
Andrey <a5kin@users.noreply.github.com>
2017-10-09 00:51:24 +0300

    TorusBorder is implemented.

--- hecate/core/topology/border.py
+++ hecate/core/topology/border.py
@@ -6,7 +6,18 @@ class Border(DimensionsMixin):
     Base class for all types of borders.
 
     """
+    def __init__(self):
+        self.topology = None
 
 
 class TorusBorder(Border):
     supported_dimensions = [2, ]
+
+    def wrap_coords(self, coord_prefix):
+        code = ""
+        for i in range(self.dimensions):
+            code += "{x}{i} %= {w}{i};\n".format(
+                x=coord_prefix, i=i,
+                w=self.topology.lattice.width_prefix
+            )
+        return code
