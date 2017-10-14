c638e34ddd07cea28c923204050be3b51a2f6e65
Andrey <a5kin@users.noreply.github.com>
2017-10-14 17:58:32 +0300

    Add placeholder for ContainerProperty

--- hecate/core/properties.py
+++ hecate/core/properties.py
@@ -13,3 +13,7 @@ class IntegerProperty(Property):
     def __init__(self, max_val):
         self.max_val = max_val
         self.bit_width = int(math.log2(max_val)) + 1
+
+
+class ContainerProperty(Property):
+    pass
