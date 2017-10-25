a21b03a42da656a0688c9da3315a6a2167a3b30c
Andrey <a5kin@users.noreply.github.com>
2017-10-26 01:16:17 +0300

    Add instance name detection for Variable (#20)

--- hecate/core/variables.py
+++ hecate/core/variables.py
@@ -1,8 +1,27 @@
+import inspect
+import itertools
+
+from cached_property import cached_property
+
+
 class Variable:
     """
     Base class for all variables.
 
     """
+    def __init__(self):
+        pass
+
+    @cached_property
+    def var_name(self):
+        frame = inspect.currentframe().f_back.f_back
+        all_vars = itertools.chain(frame.f_globals.items(),
+                                   frame.f_locals.items())
+        for k, var in all_vars:
+            if isinstance(var, self.__class__):
+                if hash(self) == hash(var):
+                    return k
+        return ''
 
 
 class IntegerVariable(Variable):
