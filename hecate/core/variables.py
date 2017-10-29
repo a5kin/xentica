589ced9d1bd96b9c22e7f1bce5016888ea8086fe
Andrey <a5kin@users.noreply.github.com>
2017-10-29 22:06:04 +0200

    Generate DeferredExpression magic methods dynamically (#20)

--- hecate/core/variables.py
+++ hecate/core/variables.py
@@ -7,48 +7,49 @@ class DeferredExpression:
 
     def __init__(self, code=''):
         self.code = code
+        ops = (
+            ('+', 'add'),
+            ('-', 'sub'),
+            ('*', 'mul'),
+            ('/', 'truediv'),
+            ('%', 'mod'),
+            ('>>', 'rshift'),
+            ('<<', 'lshift'),
+            ('&', 'and'),
+            ('~', 'xor'),
+            ('|', 'or'),
+        )
+        for c_op, base_name in ops:
+            def binary_direct(op):
+                def op_func(self_var, value):
+                    code = "(%s %s %s)" % (self_var, op, value)
+                    return DeferredExpression(code)
+                return op_func
+
+            def binary_reflected(op):
+                def op_func(self_var, value):
+                    code = "(%s %s %s)" % (value, op, self_var)
+                    return DeferredExpression(code)
+                return op_func
+
+            def augmented_assign(op):
+                def op_func(self_var, value):
+                    if isinstance(self_var, Variable):
+                        self_var._declare_once()
+                    code = "%s += %s;\n" % (self_var.var_name, value)
+                    self_var._bsca._func_body += code
+                    return self
+                return op_func
+
+            func_name = "__%s__" % base_name
+            setattr(self.__class__, func_name, binary_direct(c_op))
+            func_name = "__r%s__" % base_name
+            setattr(self.__class__, func_name, binary_reflected(c_op))
+            func_name = "__i%s__" % base_name
+            setattr(self.__class__, func_name, augmented_assign(c_op))
 
     def __str__(self):
         return self.code
-
-    def __rshift__(self, value):
-        code = "(%s >> %s)" % (self, value)
-        return DeferredExpression(code)
-
-    def __rrshift__(self, value):
-        code = "(%s >> %s)" % (value, self)
-        return DeferredExpression(code)
-
-    def __and__(self, value):
-        code = "(%s & %s)" % (self, value)
-        return DeferredExpression(code)
-
-    def __rand__(self, value):
-        code = "(%s & %s)" % (value, self)
-        return DeferredExpression(code)
-
-    def __or__(self, value):
-        code = "(%s | %s)" % (self, value)
-        return DeferredExpression(code)
-
-    def __ror__(self, value):
-        code = "(%s | %s)" % (value, self)
-        return DeferredExpression(code)
-
-    def __mul__(self, value):
-        code = "(%s * %s)" % (self, value)
-        return DeferredExpression(code)
-
-    def __rmul__(self, value):
-        code = "(%s * %s)" % (value, self)
-        return DeferredExpression(code)
-
-    def __iadd__(self, value):
-        if isinstance(self, Variable):
-            self._declare_once()
-        code = "%s += %s;\n" % (self.var_name, value)
-        self._bsca._func_body += code
-        return self
 
 
 class Variable(DeferredExpression):
