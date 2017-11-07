34d4ae7a831f47038c26acbf0110b33fbd182bcd
Andrey <a5kin@users.noreply.github.com>
2017-11-08 00:55:51 +0200

    Turn color effects decorators to classes (#20)

--- hecate/core/color_effects.py
+++ hecate/core/color_effects.py
@@ -1,28 +1,58 @@
-def color_effect(func, effect):
-    def wrapper(self_var):
-        r, g, b = func(self_var)
-        code = """
-            int new_r = %s;
-            int new_g = %s;
-            int new_b = %s;
-            %s
-            col[i] = make_int3(new_r, new_g, new_b);
-        """ % (r, g, b, effect)
-        self_var._func_body += code
-    return wrapper
+import inspect
+
+from hecate.core.variables import Constant
+from hecate.core.base import BSCA, HecateException
 
 
-def moving_average(func):
-    effect = """
-        new_r *= SMOOTH_FACTOR;
-        new_g *= SMOOTH_FACTOR;
-        new_b *= SMOOTH_FACTOR;
-        int3 old_col = col[i];
-        new_r = max(min(new_r, old_col.x + FADE_IN),
-                    old_col.x - FADE_OUT);
-        new_g = max(min(new_g, old_col.y + FADE_IN),
-                    old_col.y - FADE_OUT);
-        new_b = max(min(new_b, old_col.z + FADE_IN),
-                    old_col.z - FADE_OUT);
-    """
-    return color_effect(func, effect)
+class ColorEffect:
+
+    def __init__(self, func):
+        pass
+
+    @property
+    def _bsca(self):
+        frame = inspect.currentframe()
+        while frame is not None:
+            for l in frame.f_locals.values():
+                if hasattr(l, "__get__"):
+                    continue
+                if isinstance(l, BSCA):
+                    return l
+            frame = frame.f_back
+        raise HecateException("BSCA not detected for ColorEffect")
+
+    def __call__(self, func):
+        self._bsca._constants.add(Constant("FADE_IN", "fade_in"))
+        self._bsca._constants.add(Constant("FADE_OUT", "fade_out"))
+        self._bsca._constants.add(Constant("SMOOTH_FACTOR",
+                                           "smooth_factor"))
+
+        def wrapper(self_var):
+            r, g, b = func(self_var)
+            code = """
+                int new_r = %s;
+                int new_g = %s;
+                int new_b = %s;
+                %s
+                col[i] = make_int3(new_r, new_g, new_b);
+            """ % (r, g, b, self.effect)
+            self_var.append_code(code)
+        return wrapper
+
+
+class MovingAverage(ColorEffect):
+
+    def __call__(self, func):
+        self.effect = """
+            new_r *= SMOOTH_FACTOR;
+            new_g *= SMOOTH_FACTOR;
+            new_b *= SMOOTH_FACTOR;
+            int3 old_col = col[i];
+            new_r = max(min(new_r, old_col.x + FADE_IN),
+                        old_col.x - FADE_OUT);
+            new_g = max(min(new_g, old_col.y + FADE_IN),
+                        old_col.y - FADE_OUT);
+            new_b = max(min(new_b, old_col.z + FADE_IN),
+                        old_col.z - FADE_OUT);
+        """
+        return super(MovingAverage, self).__call__(func)
