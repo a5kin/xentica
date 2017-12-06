a4271732a0bf9dd71efd4bdf5ec3669e209677bd
Andrey <a5kin@users.noreply.github.com>
2017-12-07 00:05:55 +0200

    Add docstrings for tests.core.mixins (#23, #27)

--- tests/core/test_mixins.py
+++ tests/core/test_mixins.py
@@ -1,3 +1,4 @@
+"""Tests for ``xentica.core.mixins`` module."""
 import unittest
 
 from xentica.core.mixins import BscaDetectorMixin
@@ -5,8 +6,10 @@ from xentica.core.exceptions import XenticaException
 
 
 class TestBscaDetector(unittest.TestCase):
+    """Tests for ``BscaDetectorMixin`` class."""
 
     def test_not_detected(self):
+        """Test exception is raised if BSCA not detected."""
         class Dummy(BscaDetectorMixin):
             pass
         with self.assertRaises(XenticaException):
