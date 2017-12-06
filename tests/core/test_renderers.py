808f56cffb49ed43d24f623d248cb0900b714f8b
Andrey <a5kin@users.noreply.github.com>
2017-12-07 00:06:18 +0200

    Add docstrings for tests.core.renderers (#23, #27)

--- tests/core/test_renderers.py
+++ tests/core/test_renderers.py
@@ -1,11 +1,14 @@
+"""Tests for ``xentica.core.renderers`` module."""
 import unittest
 
 from xentica.core.renderers import Renderer
 
 
 class TestRenderer(unittest.TestCase):
+    """Tests for ``Renderer`` class."""
 
     def test_base(self):
+        """Test base class is returning empty code for kernel."""
         renderer = Renderer()
         code = renderer.render_code()
         self.assertEqual(code, "", "Base class kernel should be empty")
