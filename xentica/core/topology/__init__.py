ccf675b7d679ebc6672092c2beee1b1298f648af
Andrey <a5kin@users.noreply.github.com>
2018-01-02 14:41:01 +0200

    Add modules cross-reference (#23, #27)

--- xentica/core/topology/__init__.py
+++ xentica/core/topology/__init__.py
@@ -1,20 +1,20 @@
 """This package helps you build the topology for CA models.
 
-All ``CellularAutomaton`` subclasses **must** have ``Topology`` class
-declared inside. This class describes:
+All :class:`xentica.core.CellularAutomaton` subclasses **must** have
+``Topology`` class declared inside. This class describes:
 
 - ``dimensions``: the number of dimensions your CA model operates on.
 
 - ``lattice``: the type of lattice of your CA board. Built-in lattice
-  types are available in ``core.topology.lattice`` module.
+  types are available in :mod:`xentica.core.topology.lattice` module.
 
 - ``neighborhood``: the type of neighborhood for a single
   cell. Built-in neighborhood types are available in
-  ``core.topology.neighborhood`` module.
+  :mod:`xentica.core.topology.neighborhood` module.
 
 - ``border``: the type of border effect, e.g. how to process off-board
   cells. Built-in border types are available in
-  ``core.topology.border`` module.
+  :mod:`xentica.core.topology.border` module.
 
 In example, you can declare the topology for a 2-dimensional
 orthogonal lattice with Moore neighborhood, wrapped to a 3-torus, as
