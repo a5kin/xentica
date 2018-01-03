7346665052eac9dbf7578728b85e4cbd16727249
Andrey <a5kin@users.noreply.github.com>
2018-01-03 18:53:51 +0200

    Add docstrings to core.mixins module (#23, #27)

--- xentica/core/mixins.py
+++ xentica/core/mixins.py
@@ -1,3 +1,10 @@
+"""
+The collection of mixins to be used in core classes.
+
+Would be interesting only if you are planning to hack into Xentica
+core functionality.
+
+"""
 import inspect
 
 import xentica.core.base
@@ -5,9 +12,21 @@ from xentica.core.exceptions import XenticaException
 
 
 class BscaDetectorMixin:
+    """
+    Add a functionlality to detect BSCA class instances holding current class.
+
+    All methods are for private use only.
+
+    """
 
     @property
     def _bsca(self):
+        """
+        Get a BSCA instance holding current class.
+
+        Objects tree is scanned up to top and first instance found is returned.
+
+        """
         frame = inspect.currentframe()
         while frame is not None:
             for l in frame.f_locals.values():
@@ -20,6 +39,12 @@ class BscaDetectorMixin:
 
     @property
     def _holder_frame(self):
+        """
+        Get a frame of class instance holding current class.
+
+        Objects tree is scanned up to top and first instance found is returned.
+
+        """
         # TODO: detect base class by scanning inheritance tree:
         # inspect.getclasstree(inspect.getmro(type(self)))
         frame = inspect.currentframe().f_back.f_back.f_back
@@ -29,4 +54,5 @@ class BscaDetectorMixin:
 
     @property
     def _holder(self):
+        """Get an instance from a frame found by :meth:`_holder_frame`."""
         return self._holder_frame.f_locals['self']
