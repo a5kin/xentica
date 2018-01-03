6f67885a12f94ec698b6f2df1120291e278683ea
Andrey <a5kin@users.noreply.github.com>
2018-01-03 23:08:42 +0200

    Add core package description (#23, #27)

--- xentica/core/__init__.py
+++ xentica/core/__init__.py
@@ -1,3 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Xentica core functionality is available via modules from this package.
+
+In addition, you may use ``core`` package as a shortcut to the main
+classes of the framework.
+
+- **Base classes**
+   - ``core.CellularAutomaton`` →
+     :class:`xentica.core.base.CellularAutomaton`
+   - ``core.Experiment`` →
+     :class:`xentica.core.experiment.Experiment`
+
+- **Lattices**
+   - ``core.OrthogonalLattice`` →
+     :class:`xentica.core.topology.lattice.OrthogonalLattice`
+
+- **Neighborhoods**
+   - ``core.MooreNeighborhood`` →
+     :class:`xentica.core.topology.neighborhood.MooreNeighborhood`
+   - ``core.VonNeumannNeighborhood`` →
+     :class:`xentica.core.topology.neighborhood.VonNeumannNeighborhood`
+
+- **Borders**
+   - ``core.TorusBorder`` →
+     :class:`xentica.core.topology.border.TorusBorder`
+   - ``core.StaticBorder`` →
+     :class:`xentica.core.topology.border.StaticBorder`
+
+- **Properties**
+   - ``core.IntegerProperty`` →
+     :class:`xentica.core.properties.IntegerProperty`
+
+- **Variables**
+   - ``core.IntegerVariable`` →
+     :class:`xentica.core.variables.IntegerVariable`
+
+The classes listed above are all you need to build CA models and
+experiments with Xentica, unless you are planning to implement custom
+core features like new lattices, borders, etc.
+
+"""
 from xentica.core.base import CellularAutomaton
 from xentica.core.properties import (
     IntegerProperty,
