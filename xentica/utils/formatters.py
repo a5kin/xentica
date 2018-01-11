58899d809cf0274c98b3e7d4c639e6a38ba337ac
Andrey <a5kin@users.noreply.github.com>
2018-01-11 22:03:26 +0200

    Add docstrings to utils.* (#23, #27)

--- xentica/utils/formatters.py
+++ xentica/utils/formatters.py
@@ -1,4 +1,21 @@
+"""The collection of formatters."""
+
+
 def sizeof_fmt(num, suffix=''):
+    """
+    Format the number with the humanized order of magnitude.
+
+    In example, ``11234`` become ``11.23K``.
+
+    :param num:
+        The positive integer.
+    :param suffix:
+        Additional suffix to add to formatted string.
+
+    :returns:
+        Formatted number as a string.
+
+    """
     if abs(num) < 1000:
         return "%s%s" % (num, suffix)
     for unit in ['K', 'M', 'G', 'T', 'P', 'E', 'Z']:
