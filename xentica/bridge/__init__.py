c2d8c368fdadbd2e7ea5764a420f6f55c6a2e386
Andrey <a5kin@users.noreply.github.com>
2018-01-14 23:33:18 +0200

    Add description of xentica.bridge package (#23, #27)

--- xentica/bridge/__init__.py
+++ xentica/bridge/__init__.py
@@ -1,3 +1,17 @@
+"""
+The bridge between Xentica and GUI interface.
+
+This package contains all necessary stuff to connect Xentica framework
+to custom interactive visualization environments.
+
+Right now, only one environment (`Moire`_) is available. This is the
+official environment, evolving along with the main framework. You are
+free to implement your own environments. If so, please make a PR on
+Github and we'll include your solution to the bridge.
+
+.. _Moire: https://github.com/a5kin/moire
+
+"""
 from xentica.bridge.moire import MoireBridge
 
 
