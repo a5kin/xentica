1dce29fa10f268da053a64cc17ca008775576756
Andrey <a5kin@users.noreply.github.com>
2018-02-02 23:19:51 +0200

    Add description to seeds package (#23, #27)

--- xentica/seeds/__init__.py
+++ xentica/seeds/__init__.py
@@ -1,3 +1,23 @@
+"""
+The package helping you to construct initial CA state (seed).
+
+Classes from modules below are intended for use in
+:class:`Experiment <xentica.core.experiments.Experiment>` classes.
+
+For example to initialize the whole board with random values::
+
+    from xentica import core, seeds
+
+    class MyExperiment(core.Experiment):
+        # ...
+        seed = seeds.patterns.PrimordialSoup(
+            vals={
+                "state": seeds.random.RandInt(0, 1),
+            }
+       )
+
+"""
+
 from xentica.seeds import random
 from xentica.seeds import patterns
 
