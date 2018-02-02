f7f9cbfb27c868cca04cf449dc04a06c5e146b7d
Andrey <a5kin@users.noreply.github.com>
2018-02-02 22:52:36 +0200

    Add docstrings for bridge.base module (#23, #27)

--- xentica/bridge/base.py
+++ xentica/bridge/base.py
@@ -1,23 +1,36 @@
+"""
+This module contains the main class to be used in custom bridges.
+
+Methods from :class:`Bridge` class should be used in other bridges
+
+"""
+
+
 class Bridge:
+    """Main bridge class containing basic functions."""
 
     @staticmethod
     def noop(ca, gui):
-        pass
+        """Do nothing."""
 
     @staticmethod
     def exit_app(ca, gui):
+        """Exit GUI application."""
         gui.exit_app()
 
     @staticmethod
     def speed(dspeed):
+        """Change simulation speed."""
         def func(ca, gui):
             ca.apply_speed(dspeed)
         return func
 
     @staticmethod
     def toggle_pause(ca, gui):
+        """Pause/unpause simulation."""
         ca.toggle_pause()
 
     @staticmethod
     def toggle_sysinfo(ca, gui):
+        """Turn system info panel on/off."""
         gui.sysinfo.toggle()
