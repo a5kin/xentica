c035958565607d948010a11ea16776fdcbc7c535
Andrey <a5kin@users.noreply.github.com>
2018-02-18 09:54:27 +0200

    Add docstring to main xentica package (#23, #27)

---
+++ xentica/__init__.py
@@ -0,0 +1,8 @@
+"""
+The Python CA engine.
+
+Xentica is the framework to build GPU-accelerated models for
+multi-dimensional cellular automata. Given pure Python definitions, it
+generates kernels in CUDA C and runs them on NVIDIA hardware.
+
+"""
