3029cdb77aab3eac66ba2663437016925238f701
Andrey <a5kin@users.noreply.github.com>
2018-02-25 21:30:52 +0200

    Add package setup script (#23)

---
+++ setup.py
@@ -0,0 +1,56 @@
+"""
+A setuptools based setup module.
+
+Adopted from:
+https://github.com/pypa/sampleproject
+
+"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='xentica',
+    version='0.1.0',
+    description='GPU-accelerated engine for multi-dimensional '
+                'cellular automata',
+    long_description=long_description,
+    url='https://github.com/a5kin/xentica',
+    author='Andrey Zamaraev',
+    author_email='a5kin@github.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Games/Entertainment :: Simulation',
+        'Topic :: Scientific/Engineering :: Artificial Life',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Artistic Software',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    keywords='cellular automata ca simulation artificial life alife energy '
+             'framework gpu multidimensional microprograms conservation',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=[
+        'numpy',
+        'pycuda',
+        'cached-property',
+    ],
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+)
