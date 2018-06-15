63262a3c2b10bd1f2aa5d8407dfcbb28284f9084
Andrey <a5kin@users.noreply.github.com>
2018-06-15 23:52:38 +0300

    Fix pylint issues in tests.bridge.test_base (#30)

--- tests/bridge/test_base.py
+++ tests/bridge/test_base.py
@@ -29,79 +29,79 @@ class TestBridge(unittest.TestCase):
 
     def setUp(self):
         """Set up necessary things before each test."""
-        self.ca = GameOfLife(GOLExperiment)
+        self.model = GameOfLife(GOLExperiment)
         self.gui = DummyGui()
 
     def test_noop(self):
         """Test no-op action."""
-        Bridge.noop(self.ca, self.gui)
+        Bridge.noop(self.model, self.gui)
 
     def test_exit(self):
         """Test exit action."""
-        Bridge.exit_app(self.ca, self.gui)
+        Bridge.exit_app(self.model, self.gui)
 
     def test_move_up(self):
         """Test move up action."""
-        y = self.ca.pos[1]
-        move_up = self.ca.bridge.key_actions['up']
-        move_up(self.ca, self.gui)
-        self.assertEqual((y + 1) % self.ca.size[1], self.ca.pos[1])
+        coord_y = self.model.pos[1]
+        move_up = self.model.bridge.key_actions['up']
+        move_up(self.model, self.gui)
+        self.assertEqual((coord_y + 1) % self.model.size[1], self.model.pos[1])
 
     def test_move_down(self):
         """Test move down action."""
-        y = self.ca.pos[1]
-        move_down = self.ca.bridge.key_actions['down']
-        move_down(self.ca, self.gui)
-        self.assertEqual((y - 1) % self.ca.size[1], self.ca.pos[1])
+        coord_y = self.model.pos[1]
+        move_down = self.model.bridge.key_actions['down']
+        move_down(self.model, self.gui)
+        self.assertEqual((coord_y - 1) % self.model.size[1], self.model.pos[1])
 
     def test_move_right(self):
         """Test move right action."""
-        x = self.ca.pos[0]
-        move_right = self.ca.bridge.key_actions['right']
-        move_right(self.ca, self.gui)
-        self.assertEqual((x + 1) % self.ca.size[0], self.ca.pos[0])
+        coord_x = self.model.pos[0]
+        move_right = self.model.bridge.key_actions['right']
+        move_right(self.model, self.gui)
+        self.assertEqual((coord_x + 1) % self.model.size[0], self.model.pos[0])
 
     def test_move_left(self):
         """Test move left action."""
-        x = self.ca.pos[0]
-        move_left = self.ca.bridge.key_actions['left']
-        move_left(self.ca, self.gui)
-        self.assertEqual((x - 1) % self.ca.size[0], self.ca.pos[0])
+        coord_x = self.model.pos[0]
+        move_left = self.model.bridge.key_actions['left']
+        move_left(self.model, self.gui)
+        self.assertEqual((coord_x - 1) % self.model.size[0], self.model.pos[0])
 
     def test_zoom_in(self):
         """Test zoom in action."""
-        zoom = self.ca.zoom
-        zoom_in = self.ca.bridge.key_actions['=']
-        zoom_in(self.ca, self.gui)
-        self.assertEqual(zoom + 1, self.ca.zoom)
+        zoom = self.model.zoom
+        zoom_in = self.model.bridge.key_actions['=']
+        zoom_in(self.model, self.gui)
+        self.assertEqual(zoom + 1, self.model.zoom)
 
     def test_zoom_out(self):
         """Test zoom out action."""
-        zoom = self.ca.zoom
-        zoom_out = self.ca.bridge.key_actions['-']
-        zoom_out(self.ca, self.gui)
-        self.assertEqual(max(zoom - 1, 1), self.ca.zoom)
+        zoom = self.model.zoom
+        zoom_out = self.model.bridge.key_actions['-']
+        zoom_out(self.model, self.gui)
+        self.assertEqual(max(zoom - 1, 1), self.model.zoom)
 
     def test_speed_up(self):
         """Test speed up action."""
-        speed = self.ca.speed
-        speed_up = self.ca.bridge.key_actions[']']
-        speed_up(self.ca, self.gui)
-        self.assertEqual(speed + 1, self.ca.speed)
+        speed = self.model.speed
+        speed_up = self.model.bridge.key_actions[']']
+        speed_up(self.model, self.gui)
+        self.assertEqual(speed + 1, self.model.speed)
 
     def test_speed_down(self):
         """Test speed down action."""
-        speed = self.ca.speed
-        speed_down = self.ca.bridge.key_actions['[']
-        speed_down(self.ca, self.gui)
-        self.assertEqual(max(speed - 1, 1), self.ca.speed)
+        speed = self.model.speed
+        speed_down = self.model.bridge.key_actions['[']
+        speed_down(self.model, self.gui)
+        self.assertEqual(max(speed - 1, 1), self.model.speed)
 
     def test_toggle_pause(self):
         """Test toggle pause action."""
-        paused = self.ca.paused
-        Bridge.toggle_pause(self.ca, self.gui)
-        self.assertEqual(not paused, self.ca.paused)
+        paused = self.model.paused
+        Bridge.toggle_pause(self.model, self.gui)
+        self.assertEqual(not paused, self.model.paused)
 
     def test_toggle_sysinfo(self):
         """Test toggle sysinfo action."""
-        Bridge.toggle_sysinfo(self.ca, self.gui)
+        Bridge.toggle_sysinfo(self.model, self.gui)
