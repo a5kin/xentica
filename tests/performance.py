5cb98de0e8a3a67baf214f59328498c695b022de
Andrey <a5kin@users.noreply.github.com>
2018-06-15 23:55:25 +0300

    Fix pylint issues in tests.performance (#30)

--- tests/performance.py
+++ tests/performance.py
@@ -5,19 +5,19 @@ from examples.game_of_life import GameOfLife, GOLExperiment
 from xentica.utils.formatters import sizeof_fmt
 
 
-models = [
+MODELS = [
     ("Conway's Life", GameOfLife, GOLExperiment),
 ]
-num_steps = 10000
+NUM_STEPS = 10000
 
 
 if __name__ == "__main__":
-    for name, model, experiment in models:
+    for name, model, experiment in MODELS:
         ca = model(experiment)
         start_time = time.time()
-        for j in range(num_steps):
+        for j in range(NUM_STEPS):
             ca.step()
             time_passed = time.time() - start_time
-        speed = num_steps * ca.cells_num // time_passed
+        speed = NUM_STEPS * ca.cells_num // time_passed
         print("%s: %s cells/s" % (name, sizeof_fmt(speed)))
         del ca
