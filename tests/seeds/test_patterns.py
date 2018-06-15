2aa2c705cbd8063d0f4aa4d2f8cc5ec09d8ed577
Andrey <a5kin@users.noreply.github.com>
2018-06-15 22:16:00 +0300

    Fix pylint issues in tests.seeds.test_patterns (#30)

--- tests/seeds/test_patterns.py
+++ tests/seeds/test_patterns.py
@@ -15,69 +15,68 @@ class TestValDict(unittest.TestCase):
 
     def test_constant_value(self):
         """Test constant value storing."""
-        d = {'s': 3}
-        vd = ValDict(d)
-        self.assertEqual(vd['s'], 3, "Wrong constant value.")
+        val_dict = ValDict({'s': 3})
+        self.assertEqual(val_dict['s'], 3, "Wrong constant value.")
 
     def test_random_value(self):
         """Test random value storing."""
-        d = {'s': RandInt(11, 23)}
-        vd = ValDict(d)
-        for i in range(10):
-            self.assertGreaterEqual(vd['s'], 11, "Wrong random value.")
-            self.assertLessEqual(vd['s'], 23, "Wrong random value.")
+        val_dict = ValDict({'s': RandInt(11, 23)})
+        for _ in range(10):
+            self.assertGreaterEqual(val_dict['s'], 11, "Wrong random value.")
+            self.assertLessEqual(val_dict['s'], 23, "Wrong random value.")
 
     def test_multiple_values(self):
         """Test multiple values storing."""
-        d = {'a': 2, 's': RandInt(11, 23), 'd': 3.3}
-        vd = ValDict(d)
-        self.assertEqual(vd['a'], 2, "Wrong first constant value.")
-        for i in range(10):
-            self.assertGreaterEqual(vd['s'], 11, "Wrong random value.")
-            self.assertLessEqual(vd['s'], 23, "Wrong random value.")
-        self.assertEqual(vd['d'], 3.3, "Wrong second constant value.")
+        val_dict = ValDict({'a': 2, 's': RandInt(11, 23), 'd': 3.3})
+        self.assertEqual(val_dict['a'], 2, "Wrong first constant value.")
+        for _ in range(10):
+            self.assertGreaterEqual(val_dict['s'], 11, "Wrong random value.")
+            self.assertLessEqual(val_dict['s'], 23, "Wrong random value.")
+        self.assertEqual(val_dict['d'], 3.3, "Wrong second constant value.")
 
     def test_multiple_dicts(self):
         """Test different dicts keeping different values."""
-        vd1 = ValDict({'s': 2})
-        vd2 = ValDict({'s': RandInt(11, 23)})
-        self.assertEqual(vd1['s'], 2, "Wrong first ValDict.")
-        for i in range(10):
-            self.assertGreaterEqual(vd2['s'], 11, "Wrong second ValDict.")
-            self.assertLessEqual(vd2['s'], 23, "Wrong second ValDict.")
+        val_dict1 = ValDict({'s': 2})
+        val_dict2 = ValDict({'s': RandInt(11, 23)})
+        self.assertEqual(val_dict1['s'], 2, "Wrong first ValDict.")
+        for _ in range(10):
+            self.assertGreaterEqual(val_dict2['s'], 11,
+                                    "Wrong second ValDict.")
+            self.assertLessEqual(val_dict2['s'], 23, "Wrong second ValDict.")
 
     def test_incorrect_key(self):
         """Test incorrect key access."""
-        vd = ValDict({'s': 2})
+        val_dict = ValDict({'s': 2})
         with self.assertRaises(KeyError):
-            vd['a']
+            val_dict['s'] = val_dict['a']
 
     def test_items(self):
         """Test iteration over items."""
-        d = {'a': 2, 's': RandInt(11, 23), 'd': 3.3}
-        vd = ValDict(d)
-        for k, v in vd.items():
-            if k == 'a':
-                self.assertEqual(v, 2, "Wrong constant value.")
-            elif k == 's':
-                self.assertGreaterEqual(v, 11, "Wrong random value.")
-                self.assertLessEqual(v, 23, "Wrong random value.")
+        val_dict = ValDict({'a': 2, 's': RandInt(11, 23), 'd': 3.3})
+        for key, val in val_dict.items():
+            if key == 'a':
+                self.assertEqual(val, 2, "Wrong constant value.")
+            elif key == 's':
+                self.assertGreaterEqual(val, 11, "Wrong random value.")
+                self.assertLessEqual(val, 23, "Wrong random value.")
 
     def test_incorrect_access(self):
         """Test item setting."""
         with self.assertRaises(NotImplementedError):
-            vd = ValDict({})
-            vd['a'] = 2
+            val_dict = ValDict({})
+            val_dict['a'] = 2
 
 
 class TestPatternBase(unittest.TestCase):
     """Base class for patterns testing."""
 
-    def index_to_coord(self, i):
+    @staticmethod
+    def index_to_coord(i):
         """Emulate ``CellularAutomaton.index_to_coord`` behavior."""
         return (i % 100, i // 100)
 
-    def pack_state(self, state):
+    @staticmethod
+    def pack_state(state):
         """Emulate ``CellularAutomaton.pack_state`` behavior."""
         return state['s']
 
@@ -104,45 +103,50 @@ class TestBigBang(TestPatternBase):
         pos = (32, 20)
         size = (10, 10)
         for i in np.where(cells == 1)[0]:
-            x, y = self.index_to_coord(i)
-            self.assertGreaterEqual(x, pos[0], "Wrong right bound.")
-            self.assertGreaterEqual(y, pos[1], "Wrong upper bound.")
-            self.assertLessEqual(x, size[0] + pos[0], "Wrong left bound.")
-            self.assertLessEqual(y, size[1] + pos[1], "Wrong lower bound.")
+            coord_x, coord_y = self.index_to_coord(i)
+            self.assertGreaterEqual(coord_x, pos[0], "Wrong right bound.")
+            self.assertGreaterEqual(coord_y, pos[1], "Wrong upper bound.")
+            self.assertLessEqual(coord_x, size[0] + pos[0],
+                                 "Wrong left bound.")
+            self.assertLessEqual(coord_y, size[1] + pos[1],
+                                 "Wrong lower bound.")
 
     def test_random_size(self):
         """Test omitted size behavior."""
         bsca = GameOfLife(TestExperiment)
         vals = {'state': RandInt(0, 1)}
-        bb = BigBang(pos=(10, 10), vals=vals)
+        pattern = BigBang(pos=(10, 10), vals=vals)
         cells = np.zeros((10000, ), dtype=np.int32)
-        bb.generate(cells, bsca)
+        pattern.generate(cells, bsca)
+        self.assertGreater(np.sum(cells), 0, "Empty board.")
 
     def test_random_pos(self):
         """Test omitted position behavior."""
         bsca = GameOfLife(TestExperiment)
         vals = {'state': RandInt(0, 1)}
-        bb = BigBang(size=(10, 10), vals=vals)
+        pattern = BigBang(size=(10, 10), vals=vals)
         cells = np.zeros((10000, ), dtype=np.int32)
-        bb.generate(cells, bsca)
+        pattern.generate(cells, bsca)
+        self.assertGreater(np.sum(cells), 0, "Empty board.")
 
     def test_random_full(self):
         """Test omitted size and position behavior."""
         bsca = GameOfLife(TestExperiment)
         vals = {'state': RandInt(0, 1)}
-        bb = BigBang(vals=vals)
+        pattern = BigBang(vals=vals)
         cells = np.zeros((10000, ), dtype=np.int32)
-        bb.generate(cells, bsca)
+        pattern.generate(cells, bsca)
+        self.assertGreater(np.sum(cells), 0, "Empty board.")
 
     def test_wrong_pos(self):
         """Test position auto-correction to make area fit to field."""
         bsca = GameOfLife(TestExperiment)
         vals = {'state': RandInt(0, 1)}
-        bb = BigBang(pos=(90, 90), size=(20, 20), vals=vals)
+        pattern = BigBang(pos=(90, 90), size=(20, 20), vals=vals)
         cells = np.zeros((10000, ), dtype=np.int32)
-        bb.generate(cells, bsca)
-        self.assertEqual(bb.pos[0], 80, "Wrong X position.")
-        self.assertEqual(bb.pos[1], 80, "Wrong Y position.")
+        pattern.generate(cells, bsca)
+        self.assertEqual(pattern.pos[0], 80, "Wrong X position.")
+        self.assertEqual(pattern.pos[1], 80, "Wrong Y position.")
 
 
 class TestPrimordialSoup(TestPatternBase):
