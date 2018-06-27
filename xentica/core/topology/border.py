4c2c0bbdb3b7c1e72cf8259b40ad6367e0d984f3
Andrey <a5kin@users.noreply.github.com>
2018-06-27 03:14:15 +0300

    Fix pylint issues in core.topology.border (#30)

--- xentica/core/topology/border.py
+++ xentica/core/topology/border.py
@@ -37,6 +37,7 @@ class Border(DimensionsMixin):
     def __init__(self):
         """Initialize common things for all borders."""
         self.topology = None
+        super(Border, self).__init__()
 
 
 class WrappedBorder(Border):
@@ -138,6 +139,7 @@ class StaticBorder(GeneratedBorder):
 
         """
         self._value = value
+        super(StaticBorder, self).__init__()
 
     def off_board_state(self, coord_prefix):
         """
