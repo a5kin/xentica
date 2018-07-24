07c7cd6215570145c9245f9e66ca9a9308c18057
Andrey <a5kin@users.noreply.github.com>
2018-07-24 23:46:52 +0300

    Fix version in setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_long_description():
 
 setup(
     name='xentica',
-    version='0.1.0',
+    version='0.1.1',
     description='GPU-accelerated engine for multi-dimensional '
                 'cellular automata',
     long_description=get_long_description(),
