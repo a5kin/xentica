74fec13ce5ca0f011c8970e7664727b422597d9a
Andrey Zamaraev <a5kin@users.noreply.github.com>
2019-03-08 00:51:28 +0300

    Add tools.genetics module (#41, #36))

---
+++ xentica/tools/genetics.py
@@ -0,0 +1,13 @@
+"""A collection of functions allowing genetics manipulations."""
+
+
+def genome_crossover(*genomes):
+    """
+    Crossover given genomes in stochastic way.
+
+    :param genomes: A list of genomes (integers) to crossover
+
+    :returns: Single integer, a resulting genome.
+
+    """
+    raise NotImplementedError
