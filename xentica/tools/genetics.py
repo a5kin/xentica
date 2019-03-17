fb6aa002e13a1d1205da28b20d419419067117f6
Andrey Zamaraev <a5kin@users.noreply.github.com>
2019-03-17 10:30:16 +0300

    Implement basic genome crossover (#44, #36)

--- xentica/tools/genetics.py
+++ xentica/tools/genetics.py
@@ -1,13 +1,32 @@
 """A collection of functions allowing genetics manipulations."""
 
+from xentica import core
+from xentica.tools import xmath
 
-def genome_crossover(*genomes):
+
+def genome_crossover(state, num_genes, *genomes, rng_name="rng"):
     """
     Crossover given genomes in stochastic way.
 
-    :param genomes: A list of genomes (integers) to crossover
+    :param state:
+        A container holding model's properties.
+    :param num_genes:
+        Genome length, assuming all genomes has same number of genes.
+    :param genomes:
+        A list of genomes (integers) to crossover
+    :param rng_name:
+        Name of ``RandomProperty``.
 
     :returns: Single integer, a resulting genome.
 
     """
-    raise NotImplementedError
+    gene_choose = core.IntegerVariable()
+    new_genome = core.IntegerVariable()
+    for gene in range(num_genes):
+        gene_choose *= 0
+        for i, genome in enumerate(genomes):
+            gene_choose += ((genome >> gene) & 1) << i
+        rand_val = getattr(state, rng_name).uniform
+        winner_gene = xmath.int(rand_val * (len(genomes) + 1))
+        new_genome += ((gene_choose >> winner_gene) & 1) << gene
+    return new_genome
