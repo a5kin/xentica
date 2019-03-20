ecf65b980aef338e8034bb4c7a47699a1d1879a8
Andrey Zamaraev <a5kin@users.noreply.github.com>
2019-03-21 01:29:38 +0300

    Fix genome crossover (#44, #36)

--- xentica/tools/genetics.py
+++ xentica/tools/genetics.py
@@ -22,11 +22,14 @@ def genome_crossover(state, num_genes, *genomes, rng_name="rng"):
     """
     gene_choose = core.IntegerVariable()
     new_genome = core.IntegerVariable()
+    num_genomes = core.IntegerVariable()
     for gene in range(num_genes):
         gene_choose *= 0
+        num_genomes *= 0
         for i, genome in enumerate(genomes):
-            gene_choose += ((genome >> gene) & 1) << i
+            gene_choose += ((genome >> gene) & 1 & (genome > 0)) << num_genomes
+            num_genomes += (genome > 0)
         rand_val = getattr(state, rng_name).uniform
-        winner_gene = xmath.int(rand_val * (len(genomes) + 1))
+        winner_gene = xmath.int(rand_val * num_genomes)
         new_genome += ((gene_choose >> winner_gene) & 1) << gene
     return new_genome
