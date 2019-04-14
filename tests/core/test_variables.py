b4e128b9a2c7f2b10694e7bc58169ef99a25cf85
Andrey Zamaraev <a5kin@users.noreply.github.com>
2019-04-14 17:37:51 +0300

    Fix test for variable as class descriptor (#28)

--- tests/core/test_variables.py
+++ tests/core/test_variables.py
@@ -10,6 +10,9 @@ from xentica import seeds
 class InvertCA(core.CellularAutomaton):
     """CA that inverts each cell's value each step."""
     state = core.IntegerProperty(max_val=1)
+    # vars should be declared at the class level,
+    # in order to use them in direct assigns
+    intvar = IntegerVariable()
 
     class Topology:
         """Most common topology."""
@@ -20,13 +23,12 @@ class InvertCA(core.CellularAutomaton):
 
     def emit(self):
         """Do nothing at emit phase."""
+        self.intvar = self.main.state
+        self.buffers[0].state = self.intvar
 
     def absorb(self):
         """Invert cell's value."""
-        # really weird way to declare variables
-        # but at least it work with current system
-        self.__class__.intvar = IntegerVariable()
-        self.intvar = self.main.state
+        self.intvar = self.buffers[0].state
         self.main.state = -self.intvar
 
     def color(self):
@@ -36,6 +38,7 @@ class InvertCA(core.CellularAutomaton):
 class InvertExperiment(core.Experiment):
     """Test experiment for ``InvertCA``."""
 
+    word = "INSIDE OUT"
     size = (64, 36, )
     seed = seeds.patterns.PrimordialSoup(
         vals={
@@ -55,8 +58,33 @@ class TestVariable(unittest.TestCase):
     def test_descriptor(self):
         """Test ``Variable`` acting as class descriptor."""
 
-        self.assertEqual(InvertCA.__name__, "InvertCA",
-                         "Wrong class name.")
+        model = InvertCA(InvertExperiment)
+        correct_emit = """
+unsigned char _cell_state;
+_cell_state = (_cell) & 1;
+unsigned int intvar = 0;
+intvar = _cell_state;
+unsigned char _bcell_state0;
+_bcell_state0 = intvar;
+unsigned char _bcell0;
+_bcell0 = ((unsigned char) _bcell_state0 & 1);
+fld[i + n * 1] = _bcell0;
+"""
+        correct_absorb = """
+unsigned char _bcell_state0;
+_bcell_state0 = (_bcell0) & 1;
+unsigned int intvar = 0;
+intvar = _bcell_state0;
+unsigned char _cell_state;
+_cell_state = (-(intvar));
+unsigned char _cell = fld[i];
+_cell = ((unsigned char) _cell_state & 1);
+fld[i] = _cell;
+"""
+        self.assertIn(correct_emit, model.cuda_source,
+                      "Wrong code for emit().")
+        self.assertIn(correct_absorb, model.cuda_source,
+                      "Wrong code for absorb().")
 
     def test_illegal_assign(self):
         """Test illegal assign to ``DeferredExpression``."""
