669cdd9d43740609f60729f0aa7cd6d481489ff5
Andrey Zamaraev <a5kin@users.noreply.github.com>
2019-06-01 23:19:44 +0300

    Add max genes limit to genome crossover (#55)

--- xentica/tools/genetics.py
+++ xentica/tools/genetics.py
@@ -4,7 +4,7 @@ from xentica import core
 from xentica.tools import xmath
 
 
-def genome_crossover(state, num_genes, *genomes,
+def genome_crossover(state, num_genes, *genomes, max_genes=None,
                      mutation_prob=0, rng_name="rng"):
     """
     Crossover given genomes in stochastic way.
@@ -15,6 +15,8 @@ def genome_crossover(state, num_genes, *genomes,
         Genome length, assuming all genomes has same number of genes.
     :param genomes:
         A list of genomes (integers) to crossover
+    :param max_genes:
+        Upper limit for '1' genes in resulting genome.
     :param mutation_prob:
         Probability of single gene mutation.
     :param rng_name:
@@ -26,15 +28,27 @@ def genome_crossover(state, num_genes, *genomes,
     gene_choose = core.IntegerVariable()
     new_genome = core.IntegerVariable()
     num_genomes = core.IntegerVariable()
+    if max_genes is not None:
+        num_active = core.IntegerVariable()
+    new_gene = core.IntegerVariable()
+    rand_val = getattr(state, rng_name).uniform
+    start_gene = core.IntegerVariable()
+    start_gene *= 0
+    start_gene += xmath.int(rand_val * num_genes) % len(genomes)
     for gene in range(num_genes):
         gene_choose *= 0
         num_genomes *= 0
+        gene = (gene + start_gene) % num_genes
         for genome in genomes:
             gene_choose += ((genome >> gene) & 1 & (genome > 0)) << num_genomes
             num_genomes += (genome > 0)
         rand_val = getattr(state, rng_name).uniform
         winner_gene = xmath.int(rand_val * num_genomes)
-        new_gene = ((gene_choose >> winner_gene) & 1)
+        new_gene *= 0
+        new_gene += ((gene_choose >> winner_gene) & 1)
+        if max_genes is not None:
+            num_active += new_gene
+            new_gene *= num_active <= max_genes
         is_mutated = 0
         if mutation_prob > 0:
             is_mutated = getattr(state, rng_name).uniform < mutation_prob
